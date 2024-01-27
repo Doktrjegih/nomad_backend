d9f38d44a18ea48a2de56ec5ddda542f165da190
doktrjegih <doktrjegih@gmail.com>
2024-01-27 10:01:33 +0400

    location doesn't need player object anymore

--- game/location.py
+++ game/location.py
@@ -1,18 +1,16 @@
 import random
-from player import Player
 
 HOSTILE = ["mountains", "forest", "cave"]
 PEACEFUL = ["village", "river"]
 
 
 class Location:
-    def __init__(self, type_, player: Player, turn_without_tavern=0) -> None:
+    def __init__(self, type_, player_luck: int, turn_without_tavern=0) -> None:
         self.type = type_
         self.enemies = False
         self.tavern = False
         self.chest = False
         self.npc = False
-        self.player = player
 
         if self.type == 'hostile':
             self.enemies = True if random.randint(1, 10) > 5 else False
@@ -29,5 +27,5 @@ class Location:
             self.tavern = True
 
         if not self.tavern and not self.enemies:
-            if random.randint(1, 100) + self.player.luck * 2 > 95:
+            if random.randint(1, 100) + player_luck * 2 > 95:
                 self.chest = True
