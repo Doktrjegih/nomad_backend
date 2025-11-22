21a5bac3357980e56a200cc649e758f63ef108f1
Replit user <>
2025-11-22 20:24:15 +0400

    Revert "fix bug #46 (finish plot quest)"
    This reverts commit a54e87ea3942540f98ff82ef90af2e3947cf9541.

--- tavern.py
+++ tavern.py
@@ -262,10 +262,9 @@ class Tavern:
                     self.player.plot_stage += 1
                     quest.close_quest(quests=quests, player=self.player)
                     self.player.improve_stats()
-                    break
-            else:
-                print("Come back when you've finished")
-                return
+                else:
+                    print("Come back when you've finished")
+                    return
 
         # if there is no active quest
         with open(PLOT_QUESTS, "r", encoding="utf-8") as fd:
